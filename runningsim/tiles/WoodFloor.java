a908db6114e36f9e48a6740f220e4957fd6c4479
Michael Schmauderer <66334164+MSchmaud@users.noreply.github.com>
2021-02-02 23:23:03 -0600

    Add files via upload

---
+++ runningsim/tiles/WoodFloor.java
@@ -0,0 +1,17 @@
+package us.mschmaud.runningsim.tiles;
+
+import us.mschmaud.runningsim.displayaudio.Resources;
+
+/**
+ * 
+ * @author Michael Schmauderer
+ *
+ */
+
+public class WoodFloor extends Tile {
+
+	public WoodFloor(int id) {
+		super(Resources.woodFloor, id);
+	}
+	
+}
